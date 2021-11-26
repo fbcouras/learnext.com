91ae58e7b2e199ee0b980b2138e85eac28c70f9d
Fernando Couras <fbcouras@gmail.com>
2021-11-26 15:45:09 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+
+function Home() {
+    return (
+        <div className="body">
+            <h1>Home</h1>
+            <Contador />
+        </div>
+    )
+}
+
+function Contador() {
+    const [contador, setContador] = useState(1);
+    function adicionarContador() {
+        setContador(contador + 1);
+    }
+
+    return (
+        <div>
+            <div>{ contador }</div>
+            <button onClick={adicionarContador}>Adiciona Contador</button>
+        </div>
+    );
+}
+
+export default Home;
